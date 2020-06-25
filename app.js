3a9c8b748cdb7f6eb83fa7f04f5969db4d9e0c07
Beowulfdgo <antonioflores30@hotmail.com>
2020-06-25 16:14:41 -0500

    make it better

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//app.get('/', (req, res) => {
+ // res.send('Hello World!');
+//});
 
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback');

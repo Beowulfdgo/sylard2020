ea5441ab6c447dc482873a8b1f56c8798e24434b
Beowulfdgo <antonioflores30@hotmail.com>
2020-06-25 17:59:05 -0500

    configurando mongo

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ app.use(express.urlencoded({ extended: true }));
  // res.send('Hello World!');
 //});
 
+// Conexión base de datos
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017/myapp';
+const options = {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true};
+
+// Or using promises
+mongoose.connect(uri, options).then(
+  /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+  () => { console.log('Conectado a DB') },
+  /** handle initial connection error */
+  err => { console.log(err) }
+);
+
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback');
 app.use(history());
